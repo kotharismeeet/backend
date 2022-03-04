9dd7602ea26963fae22fe4acfe601371104a14a9
kotharismeeet <smeetkothari@FVFGW1AYQ05N.local>
2022-03-04 11:16:41 +0530

    Files renamed

--- controllers/Item.js
+++ controllers/Item.js
@@ -1,4 +1,4 @@
-const {VendorItem,VendorCategory,Vendor} = require('../models/vendor.js');
+const {VendorItem,VendorCategory,Vendor} = require('../models/Vendor.js');
 const asynchandler = require('express-async-handler');
 
 // /api/vendor
--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use('/api/customer',require('./routes/customerRoutes.js'));
 app.use('/api/vendor',require('./routes/vendorRoutes.js'));
-app.use('/api/item',require('./routes/vendorItem.js'));
+app.use('/api/item',require('./routes/itemRoutes.js'));
 
 app.get('/api/helloworld',(req,res) => {
     return res.send('Hello World!').sendStatus(200);
